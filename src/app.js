9c7cfdc42e2fab8aad3ce00b847fcb381b994393
Aryeh <aryehzinn@hotmail.com>
2019-11-04 09:42:11 +1100

    added more metrics

--- src/app.js
+++ src/app.js
@@ -35,6 +35,7 @@ app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
   dogstats.increment('node.page.views.index', ['method:GET', 'route:contacts']);
+  dogstats.increment('node.page.views', ['method:GET', 'route:contacts']);
   res.render('index', {
     title: 'Weather',
     name: 'Aryeh'
@@ -43,6 +44,7 @@ app.get('', (req, res) => {
 
 app.get('/about', (req, res) => {
   dogstats.increment('node.page.views.about', ['method:GET', 'route:contacts']);
+  dogstats.increment('node.page.views', ['method:GET', 'route:contacts']);
   res.render('about', {
     title: 'About',
     name: 'Aryeh'
@@ -51,6 +53,7 @@ app.get('/about', (req, res) => {
 
 app.get('/help', (req, res) => {
   dogstats.increment('node.page.views.help', ['method:GET', 'route:contacts']);
+  dogstats.increment('node.page.views', ['method:GET', 'route:contacts']);
   res.render('help', {
     title: 'Help',
     message: 'This is the help page',
@@ -81,6 +84,7 @@ app.get('/weather', (req, res) => {
 
 app.get('/help/*', (req, res) => {
   dogstats.increment('node.page.views.404', ['method:GET', 'route:contacts']);
+  dogstats.increment('node.page.views', ['method:GET', 'route:contacts']);
   dogstats.increment('node.errors', ['method:GET', 'route:contacts']);
   res.render('404', {
     title: '404',
@@ -91,6 +95,7 @@ app.get('/help/*', (req, res) => {
 
 app.get('*', (req, res) => {
   dogstats.increment('node.page.views.404', ['method:GET', 'route:contacts']);
+  dogstats.increment('node.page.views', ['method:GET', 'route:contacts']);
   dogstats.increment('node.errors', ['method:GET', 'route:contacts']);
   res.render('404', {
     title: '404',
